b3427a5e61510130364949424561f40052ac0589
Eunseo <51624588+jess8881@users.noreply.github.com>
2023-05-15 22:25:46 +0900

    (backup) 문법 오류 정정 기능만 구현

--- app.py
+++ app.py
@@ -2,51 +2,28 @@ from flask import Flask, render_template, request
 import openai
 import os
 
+#시스템 환경변수에 저장한 api key 불러오기
 openai.api_key = os.environ['OPENAI_API_KEY']
-
+#openai.api_key = "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"
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return render_template("sample.html")
 
-#html 파일 가져옴
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/submit', methods=['POST'])
-def submit():
-    user_input = request.form['user_input']
-    purpose = request.form['purpose']
-    return user_input + ' ' + purpose
-
-
-def grammar_correction(text):
-    model_engine = "text-davinci-003"
-    prompt = "Please correct my grammar: " + text
-    completions = openai.Completion.create(
-        engine=model_engine,
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.7,
+@app.route("/", methods=["POST"])
+def correct_grammar():
+    text = request.form["text"]
+    response = openai.Completion.create(
+        engine = "text-davinci-003",
+        prompt = f"Correct this to standard English.: '{text}'",
+        max_tokens = 100,
+        n = 1,
+        stop = None,
+        temperature = 0.7,
     )
-    message = completions.choices[0].text.strip()
-    return message
+    corrected_text = response.choices[0].text.strip()
+    return render_template("sample.html", corrected_text = corrected_text)
 
-def generate_recommendation(text, purpose):
-    model_engine = "text-davinci-003"
-    prompt = "Generate a recommendation for a " + purpose + " based on this text: " + text
-    completions = openai.Completion.create(
-        engine=model_engine,
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
-    message = completions.choices[0].text.strip()
-    return message
-
-
-if __name__ == "__main__":
-    app.run(debug = True)
+if __name__ == '__main__':
+    app.run(debug=True)
