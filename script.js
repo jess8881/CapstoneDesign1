b8f362bd8714dab778208207b3ddbd79c0bbc1af
Eunseo <51624588+jess8881@users.noreply.github.com>
2023-05-30 23:08:20 +0900

    (upload) UI 구현 완료

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ document.getElementById("copy").onclick = function(event) {
     result.select();
     document.execCommand('copy');
 };
+
+/* 예시 문장 번호 붙이기 */
+
+document.addEventListener('DOMContentLoaded', function() {
+  var recommendationsList = document.getElementById('list');
+  var listItems = recommendationsList.getElementsByTagName('li');
+
+  for (var i = 0; i < listItems.length; i++) {
+    var listItem = listItems[i];
+    listItem.innerText = (i + 1) + '. ' + listItem.innerText;
+  }
+});
+
